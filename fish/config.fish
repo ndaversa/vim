cb37c499766e3ee615ed6f28927efffc95077cc6
Nino D'Aversa <nino@ndaversa.com>
2016-01-28 12:13:10 -0500

    Start using oh-my-fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,8 @@
-set fish_greeting ""
-set PATH /usr/local/sbin $PATH
-set PATH /usr/local/bin $PATH
-set PATH /usr/local/opt/ruby/bin $PATH
-set PATH /opt/local/bin $PATH
-set PATH ~/bin $PATH
-set PATH "/Applications/Android Studio.app/sdk/platform-tools" $PATH
-set PATH "/Applications/Android Studio.app/sdk/tools" $PATH
-set PATH "~/.nvm" $PATH
-set MINI "mini.160913734.members.btmm.icloud.com."
-set DEV "dev"
-set WEB "web"
-set CHEF "chef-repo"
-set CAROUSELJS "carousel.js"
-set DESIGN "design"
-set MARKETING "marketing"
-set web "web"
-set fe "frontend"
-set hal "hal"
-set jira "hubot-jira-bot"
-set github "hubot-github-bot"
-hal
-boot2docker shellinit | source
-#set -x NVM_DIR ~/.nvm
-#/usr/local/opt/nvm/nvm.sh | source
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/ndaversa/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/ndaversa/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
---
+++ omf/init.fish
@@ -0,0 +1,22 @@
+set PATH /usr/local/sbin $PATH
+set PATH /usr/local/bin $PATH
+set PATH /usr/local/opt/ruby/bin $PATH
+set PATH /opt/local/bin $PATH
+set PATH ~/bin $PATH
+set PATH "/Applications/Android Studio.app/sdk/platform-tools" $PATH
+set PATH "/Applications/Android Studio.app/sdk/tools" $PATH
+set PATH "~/.nvm" $PATH
+set DEV "dev"
+set WEB "web"
+set CHEF "chef-repo"
+set CAROUSELJS "carousel.js"
+set DESIGN "design"
+set MARKETING "marketing"
+set web "web"
+set fe "frontend"
+set hal "hal"
+set jira "hubot-jira-bot"
+set github "hubot-github-bot"
+hal
+boot2docker shellinit | source
+set -x NVM_DIR ~/.nvm
